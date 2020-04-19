8dc07a90951198776cab5bf4bdf49b202092a9e2
jjlmoya <jj.lopez@avoristravel.com>
2020-04-19 09:14:55 +0200

    First commit

---
+++ layouts/Default.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="l-default l-default--background" :class="`${active ? 'is-active' : ''}`">
+        <div class="l-default__main">
+            <div class="l-default__content">
+                <slot />
+            </div>
+        </div>
+        <div class="l-default__toggle" @click="toggle"></div>
+        <Footer />
+    </div>
+</template>
+<style lang="scss">@import 'default.scss';
+</style>
+<script>
+    import Footer from '@/components/Footer/Default.vue'
+
+    export default {
+        name: 'DefaultLayout',
+        components: {
+            Footer
+        },
+        data () {
+            return {
+                active: true
+            }
+        },
+        methods: {
+            toggle () {
+                this.active = !this.active
+            }
+        }
+    }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="p-ac p-ac--crafting">
+        <Layout>
+            <div class="crafting-result">
+                <CraftCard
+                    v-for="craft in crafts"
+                    :key="craft.name"
+                    :image="craft.image"
+                    :name="craft.name"
+                    :materials="craft.materials"
+                />
+            </div>
+        </layout>
+    </div>
+</template>
+
+<style lang="scss">
+  @import 'crafting.scss';
+</style>
+
+<script>
+    import Layout from '@/layouts/Default.vue'
+    import CraftCard from '@/components/Card/CraftCard.vue'
+
+    import SeoService from '@/services/seoService'
+    import CraftingService from '@/services/games/ac/crafting.service'
+    const SEO_PAGE_DATA = new SeoService({
+        title: 'Recetas DIY - Nook Inc',
+        description: 'Listado de recetas del juego Animal Crossing New Horizons Interactivo'
+    })
+
+    export default {
+        name: 'Home',
+        components: {
+            Layout,
+            CraftCard
+        },
+        data () {
+            return {
+                crafts: this.getCrafts()
+            }
+        },
+        methods: {
+            getCrafts () {
+                return new CraftingService({}).getCrafts()
+            }
+        },
+        head () {
+            return SEO_PAGE_DATA.getMetas()
+        }
+    }
+</script>
